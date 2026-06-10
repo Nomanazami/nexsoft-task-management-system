e6227a08d36f687a2a7f1317fc0545ab59ce5b18
Noman Azami <nomanazami@users.noreply.github.com>
2026-06-10 23:51:16 +0500

    Fix: hardcode CORS origin

--- server/src/server.js
+++ server/src/server.js
@@ -22,10 +22,14 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: env.CLIENT_ORIGIN,
+    origin: [
+      'https://nexsoft-task-management-system.vercel.app',
+      'http://localhost:5173'
+    ],
     credentials: true,
   })
 );
+
 app.use(helmet());
 app.use(morgan(env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(express.json({ limit: "1mb" }));
